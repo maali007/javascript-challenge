d8c90353291194907c0f28f9c3b11606e693ba9a
maali007 <maali007@umn.edu>
2021-04-25 17:19:44 -0500

    Added code to clear filter results and reset table on clicking Reset Table button.

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -62,3 +62,28 @@ button.on("click", function() {
 });
   
 
+// LEVEL 1: PART 3 ... my extra touch ;)
+// Date/Time Filter Refresh
+// Select the button
+var button = d3.select("#refresh-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    //  Step 1: Loop Through `data` and console.log each alleged sighting object
+    allegedSightings.forEach(function(ufoClaim) {
+      console.log(ufoClaim);
+
+      // Step 2:  Use d3 to append one table row `tr` for each alleged sighting object
+        var row = tbody.append("tr");
+
+      // Step 3: Use `Object.entries` to console.log each alleged sighting value
+      Object.entries(ufoClaim).forEach(function([key, value]) {
+        console.log(key, value);
+
+        // Step 4: Use d3 to append 1 cell per alleged sighting value (datetime, city, state, country, shape, durationMinutes, comments)
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+});
