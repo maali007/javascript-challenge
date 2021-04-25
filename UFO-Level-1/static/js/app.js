b3afa3c9502e7523d839c275f844701ba5b82033
maali007 <maali007@umn.edu>
2021-04-25 15:54:09 -0500

    Use  to console.log each alleged sighting value.

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -8,8 +8,18 @@ var table = d3.select("table");
 // Use d3 to create a bootstrap striped table
 table.attr("class", "table table-striped");
 
+// Use D3 to select the table body
+var tbody = d3.select("tbody");
+
 //  Step 1: Loop Through `data` and console.log each alleged sighting object
 allegedSightings.forEach(function(ufoClaim) {
   console.log(ufoClaim);
-    // Step 2:  Use d3 to append one table row `tr` for each alleged sighting object
+
+  // Step 2:  Use d3 to append one table row `tr` for each alleged sighting object
     var row = tbody.append("tr");
+
+  // Step 3: Use `Object.entries` to console.log each alleged sighting value
+  Object.entries(ufoClaim).forEach(function([key, value]) {
+    console.log(key, value);
+
+    
